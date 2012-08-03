6f2cfeffd64fcbca1e5ffb9bc1ac1a1b80c4588e
Dan Ordille <dordille@gmail.com>
2012-08-03 15:54:24 -0400

    Initial commit

---
+++ moment.isoduration.js
@@ -0,0 +1,38 @@
+if ( typeof require !== 'undefined' )
+{
+  moment = require('moment');
+}
+
+(function(moment) {
+
+  var regex = /P((([0-9]*\.?[0-9])Y)?(([0-9]*\.?[0-9])M)?(([0-9]*\.?[0-9])W)?(([0-9]*\.?[0-9])D)?)?(T(([0-9]*\.?[0-9])H)?(([0-9]*\.?[0-9])M)?(([0-9]*\.?[0-9])S)?)?/
+
+  moment.duration.fromIsoduration = function(duration) {
+    var matches = duration.match(regex);
+
+    return moment.duration({
+      years: parseFloat(matches[3]),
+      months: parseFloat(matches[5]),
+      weeks: parseFloat(matches[7]),
+      days: parseFloat(matches[9]),
+      hours: parseFloat(matches[12]),
+      minutes: parseFloat(matches[14]),
+      seconds: parseFloat(matches[16])
+    });
+  }
+
+  moment.duration.fn.isoduration = function() {
+    var duration = 'P'
+      + ((this.years())? this.years() + 'Y' : '')
+      + ((this.months())? this.months() + 'M' : '')
+      + ((this.days())? this.days() + 'D' : '')
+      + ((this.hours() || this.minutes() || this.seconds())? 'T' : '')
+      + ((this.hours())? this.hours() + 'H' : '')
+      + ((this.minutes())? this.minutes() + 'M' : '')
+      + ((this.seconds())? this.seconds() + 'S' : '')
+    ;
+
+    return duration;
+  }
+
+})(moment);
---
+++ test/isoduration.js
@@ -0,0 +1,41 @@
+if ( typeof require !== 'undefined' )
+{
+  should = require('should')
+  moment = require('moment'),
+  require('../moment.isoduration');
+}
+
+describe('isoduration', function() {
+
+  describe('fromIsoduration', function() {
+    it('should create a moment duration from iso 8601 string with all units', function() {
+      var duration = moment.duration.fromIsoduration('P3Y6M7W4DT12H30M5S');
+      duration.years().should.equal(3);
+      duration.months().should.equal(7);
+      duration.weeks().should.equal(3);
+      duration.days().should.equal(23);
+      duration.hours().should.equal(12);
+      duration.minutes().should.equal(30);
+      duration.seconds().should.equal(5);
+    });
+
+    it('should not require YMD to be included', function() {
+      var duration = moment.duration.fromIsoduration('PT3H125M61S');
+      duration.hours().should.equal(5, 'Hours are correct');
+      duration.minutes().should.equal(6, 'Minutes are correct');
+      duration.seconds().should.equal(1, 'Seconds');
+    });
+  });
+
+  describe('isoduration', function() {
+    it('should create an iso 8601 duration from a moment duration', function() {
+      var duration = moment.duration({
+        years: 1,
+        days: 2,
+        minutes: 61
+      });
+      duration.isoduration().should.equal('P1Y2DT1H1M');
+    })
+  })
+  
+});
